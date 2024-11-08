44bafe02a1a4c497832a7ed7e6ba3ec121e2c9e5
Joseph Crump <josephcrump79@yahoo.com>
2024-11-08 04:16:29 -0600

    phase-1-static-methods-lab

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  // Static method to capitalize the first letter of a string
+  static capitalize(str) {
+    return str.slice(0,1).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+
+  // Capitalizes all words in a sentence except certain small words
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = string.split(' ');
+
+    return words
+      .map((word, index) => {
+        if (index === 0 || !exceptions.includes(word.toLowerCase())) {
+          return this.capitalize(word);
+        }
+        return word;
+      })
+      .join(' ');
+  }
 }
